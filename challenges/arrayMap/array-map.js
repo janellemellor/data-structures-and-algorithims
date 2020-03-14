2e024d70c6d8bdbf048f12e92968ba693b0f40e8
janelle mellor <janellemellor@gmail.com>
2020-03-14 15:57:45 -0700

    set up files

---
+++ challenges/arrayMap/array-map.js
@@ -0,0 +1,1 @@
+// Write a function called map which takes an array and a function as arguments. Without utilizing any of the built-in methods available to your language, return an array with the function applied to each element.
