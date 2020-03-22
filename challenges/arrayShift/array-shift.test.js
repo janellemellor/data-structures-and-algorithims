078bfc11dd0522e13ad26c5ffb335cdbd290035e
janelle mellor <janellemellor@gmail.com>
2020-03-22 10:46:26 -0700

    set up test structure

---
+++ challenges/arrayShift/array-shift.test.js
@@ -0,0 +1,22 @@
+const { insertShiftArray } = require('./array-shift.js');
+
+const array1 = [2, 4, 6, 8];
+const array2 = [4, 8, 15, 23, 42];
+
+const val1 = 5;
+const val2 = 16;
+  
+
+
+
+
+describe('arrayShift tests', () => {
+  it('', () => {
+    expect().toEqual([2, 4, 5, 6, 8]);
+  });
+
+  it('', () => {
+    expect().toEqual([4, 8, 15, 16, 23, 42]);
+  });
+
+});
