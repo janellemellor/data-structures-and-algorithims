37475e54bd48d8cf5f833f75f8e385e4ea494fdb
janelle mellor <janellemellor@gmail.com>
2020-03-22 10:41:10 -0700

    add array-shift function

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -2,12 +2,28 @@
 
 function insertShiftArray(arr, val) {
   //find the middle of the array. Should account for arrays that contain an odd number of values by rounding up to nearing whole number. 
-  const middleIndex = arr.length / 2;
+  const middleIndexPosition = Math.ceil(arr.length / 2);
 
+  //use a for loop to iterate through the array
   for(let i = 0; i < arr.length; i++) {
+  
+    //if the index is less than the middleIndexPosition, do nothing
+    if(arr[i] < middleIndexPosition) {
+      return arr[i];
+    }
+    //if the index is equal to the middleIndexPosition, insert the value
+    if(arr[i] === middleIndexPosition) {
+      return val;
+    }
+    //if the index is greater than the middleIndexPosition, move it to the next index position
+    else {
+      return arr[i] + 1;
+    }
+  } 
+  //return the insetShiftArray
+  return insertShiftArray;
+} 
 
-  }
-
-
-
-}
+module.exports = {
+  insertShiftArray
+};
