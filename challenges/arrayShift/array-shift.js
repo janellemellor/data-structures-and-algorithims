395689cc38b583be39eeb7d912bebe5b21e0dcb4
janelle mellor <janellemellor@gmail.com>
2020-03-22 10:31:19 -0700

    set up function

---
+++ challenges/arrayShift/array-shift.js
@@ -0,0 +1,13 @@
+// Write a function called insertShiftArray which takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
+
+function insertShiftArray(arr, val) {
+  //find the middle of the array. Should account for arrays that contain an odd number of values by rounding up to nearing whole number. 
+  const middleIndex = arr.length / 2;
+
+  for(let i = 0; i < arr.length; i++) {
+
+  }
+
+
+
+}
