37dca824967e123b400d99f29fa0adfc89c8cf78
janelle mellor <janellemellor@gmail.com>
2020-03-23 08:39:45 -0700

    add whiteboard comments

---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,13 @@
+// Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
+
+//arr = sorted array and search = search key
+function binarySearch(arr, search) {
+  //iterate through the sorted array
+  for(let i = 0; i < arr.length; i++) {
+    //if the search key exists in the array
+    //return the index of the element
+
+    //if the search key does not exist in the array
+    //return -1
+  }
+}
