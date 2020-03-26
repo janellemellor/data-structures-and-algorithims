ddaf698eadbbd7a50801ec7b8864997f61036497
janelle mellor <janellemellor@gmail.com>
2020-03-26 08:41:14 -0700

    fix binary search

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,21 +1,19 @@
 // Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
 
-//arr = sorted array and search = search key
+// arr = sorted array and search = search key
 function binarySearch(arr, search) {
+  //set the returned value to be -1
+  let returnValue = -1;
   //iterate through the sorted array
   for(let i = 0; i < arr.length; i++) {
     //if the search key exists in the array
-    if(arr[i] === search) {
+    if(arr[i] === search)
       //return the index of the element
-      return i;
-    }
-    //if the search key does not exist in the array
-    else if(arr[i] !== search) {
-      //return -1
-      return -1;
-    } 
-  } 
-}
+      returnValue = i;
+    //return the value
+  } return returnValue;
+} 
+
 
 module.exports = {
   binarySearch
